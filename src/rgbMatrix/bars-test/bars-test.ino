64aa639bf5b715207fb7b24bf702e841b1c4ae8e
PepoCivs <tcbpepo@gmail.com>
2019-04-20 22:55:13 +0200

    chore: improve barTest

--- src/rgbMatrix/bars-test/bars-test.ino
+++ src/rgbMatrix/bars-test/bars-test.ino
@@ -11,19 +11,38 @@
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
 
-int width = 32;
-int randomNumber = rand() % 20;
+int height = 32;
+int width = 64;
+int nBars = 3;
+int oldValues[3];
+int barWidth = width / nBars;
+int cadenceMax = 3;
+
 
 void setup() {
-
   matrix.begin();
   
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
-  matrix.drawRect(0, width-20, 10, width, matrix.Color333(0, 7, 7));
-
-  // whew!
+  for(int count = 0; count < nBars; ++count) {
+    int randomNumber = rand() % height;
+    oldValues[count] = height-randomNumber;
+  }
 }
 
 void loop() {
-
+ 
+  matrix.fillScreen(matrix.Color333(0, 0, 0)); // Put all leds black
+  for(int count = 0; count < nBars; ++count) {
+    int randomNumber = rand() % height;
+    int barHeight = height-randomNumber;
+    matrix.fillRect(count*barWidth, oldValues[count], barWidth, height, matrix.Color333(0, 1, 1));
+    matrix.drawRect(count*barWidth, oldValues[count], barWidth, height, matrix.Color333(0, 5, 5));
+    if (randomNumber%2 == 0) {
+      oldValues[count] = oldValues[count] + rand()%cadenceMax;
+    } else {
+      oldValues[count] = oldValues[count] - rand()%cadenceMax;
+    }
+    if (oldValues[count] < 0) oldValues[count] = 0;
+    if (oldValues[count] > height) oldValues[count] = height;
+  }
+  delay(100);
 }
