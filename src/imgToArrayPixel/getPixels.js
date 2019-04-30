f889c6fd0d78c8b8bc30c95799ecfc337ded6034
PepoCivs <tcbpepo@gmail.com>
2019-04-30 17:23:17 +0200

    chore: generate sortenedFrames

--- src/imgToArrayPixel/getPixels.js
+++ src/imgToArrayPixel/getPixels.js
@@ -1,10 +1,23 @@
 const Jimp = require('jimp');
 const fs = require('fs');
 
+// Helper Functions
 const getHex = (str) => {
-  return (str === 255) ? '0x000000' : `0x${str.toString(16).slice(0, -2)}`;
-}
+  return (str <= 255) ? '0x000000' : `0x${str.toString(16).slice(0, -2)}`;
+};
+const checkExtension = (fileName) => {
+  const validExt = ['png', 'jpg', 'bmp'];
+  const ext = fileName.split('.').pop();
+  return !!(validExt.indexOf(ext) > -1);
+};
+const sortByKey = (array, key) => {
+  return array.sort(function (a, b) {
+    var x = a[key]; var y = b[key];
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+};
 
+// Getter functions
 const getPixels = (image) => {
   const imageWidth = image.bitmap.width;
   const imageHeight = image.bitmap.height;
@@ -19,36 +32,47 @@ const getPixels = (image) => {
   }
   imageColors = imageColors.slice(0, -3);
   return imageColors;
-}
-
+};
 const getArrayOfPixels = (filePath) => {
   return Jimp.read(filePath)
     .then(image => getPixels(image))
     .catch(err => { throw err });
 }
 
-const checkExtension = (fileName) => {
-  const validExt = ['png', 'jpg', 'bmp'];
-  const ext = fileName.split('.').pop();
-  return !!(validExt.indexOf(ext) > -1);
-}
-
+// Core functions
+const generateFiles = (pixelImages) => {
+  Object.keys(pixelImages).map(animationName => {
+    console.log(animationName);
+    const sortedFrames = sortByKey(pixelImages[animationName], 'fileName');
+    sortedFrames.map(frame => {
+      console.log(frame.fileName);
+      console.log(frame.colorString);
+    });
+  });
+};
 const walkDirectories = function (dir, animationName = '') {
+  const promises = [];
+  const pixelImages = {};
   files = fs.readdirSync(dir);
-  files.forEach(function (fileName) {
+  files.map(function (fileName) {
     if (fs.statSync(dir + fileName).isDirectory()) {
       walkDirectories(dir + fileName + '/', fileName);
     } else if (checkExtension(fileName)) {
       const filePath = `${dir}${fileName}`;
-      console.log(animationName, filePath);
-      getArrayOfPixels(filePath).then(txt => {
-        console.log(animationName, '->', fileName);
-        console.log(txt);
-        console.log('-----');
-      });
+      promises.push(
+        getArrayOfPixels(filePath).then(colorString => {
+          if (!pixelImages[animationName]) pixelImages[animationName] = [];
+          pixelImages[animationName].push({
+            fileName,
+            colorString
+          });
+        })
+      );
     }
+  });
+  Promise.all(promises).then(() => {
+    generateFiles(pixelImages);
   });
 };
 
 walkDirectories('images/');
-// getArrayOfPixels('images/pikawalk/pika01.png').then(txt => console.log(txt));
