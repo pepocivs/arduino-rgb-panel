2e8620dcc68d673a961fdf45598830437828da01
PepoCivs <tcbpepo@gmail.com>
2019-05-04 00:58:13 +0200

    chore: back to animation string

--- src/ledStrip/animation/animation.ino
+++ src/ledStrip/animation/animation.ino
@@ -1,29 +1,19 @@
 #include <avr/pgmspace.h>  // Needed to store stuff in Flash using PROGMEM
 #include "FastLED.h" // Fastled library to control the LEDs
-#include "bulbasur.h"
-#include "emojis.h"
-#include "froslass.h"
-#include "gaysper.h"
-#include "haunter.h"
-#include "rothko.h"
-#include "triforce3D.h"
+#include "link.h"
 
 #define NUM_LEDS 525
 #define DATA_PIN 5
-#define SPEED 2000
+#define SPEED 280
 CRGB leds[NUM_LEDS];
 
 
 void setup() { 
   FastLED.addLeds<NEOPIXEL,DATA_PIN>(leds, NUM_LEDS);  // Init of the Fastled library
-  FastLED.setBrightness(80);
+  FastLED.setBrightness(10);
 }
 
 void loop() {
-  showAnimation(haunter);
-}
-
-void showAnimation(long animation[][525]) {
   for(int x = 0; x < (sizeof(animation)/sizeof(animation[0])); x++) {
     delay(SPEED);
     FastLED.clear();
