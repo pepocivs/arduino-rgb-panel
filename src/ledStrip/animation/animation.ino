a937c85a695f354db3984945fd326ca92747af6c
PepoCivs <tcbpepo@gmail.com>
2019-05-04 00:02:54 +0200

    chore: adding all animations in function

--- src/ledStrip/animation/animation.ino
+++ src/ledStrip/animation/animation.ino
@@ -1,19 +1,29 @@
 #include <avr/pgmspace.h>  // Needed to store stuff in Flash using PROGMEM
 #include "FastLED.h" // Fastled library to control the LEDs
+#include "bulbasur.h"
+#include "emojis.h"
+#include "froslass.h"
+#include "gaysper.h"
+#include "haunter.h"
 #include "rothko.h"
+#include "triforce3D.h"
 
 #define NUM_LEDS 525
 #define DATA_PIN 5
-#define SPEED 15000
+#define SPEED 2000
 CRGB leds[NUM_LEDS];
 
 
 void setup() { 
   FastLED.addLeds<NEOPIXEL,DATA_PIN>(leds, NUM_LEDS);  // Init of the Fastled library
-  FastLED.setBrightness(40);
+  FastLED.setBrightness(80);
 }
 
 void loop() {
+  showAnimation(haunter);
+}
+
+void showAnimation(long animation[][525]) {
   for(int x = 0; x < (sizeof(animation)/sizeof(animation[0])); x++) {
     delay(SPEED);
     FastLED.clear();
