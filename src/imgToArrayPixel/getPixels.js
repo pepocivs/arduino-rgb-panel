f1f76f748808b861046eda34ba9a9388f1320abc
Jorge Martín <tcbpepo@gmail.com>
2019-07-30 23:04:26 +0200

    chore: getPixels get 32BitImages and splitted for rgb panel of 32x64

--- src/imgToArrayPixel/getPixels.js
+++ src/imgToArrayPixel/getPixels.js
@@ -5,26 +5,26 @@ const configs = {
   ledMatrix: {
     imagePath: 'images/64x64/',
     zigzag: false,
-    rgb: true,
+    is32Bits: true,
+    splitImages: true,
   },
   ledStrip: {
     imagePath: 'images/25x21/',
     zigzag: true,
-    rgb: false,
+    is32Bits: false,
+    splitImages: false,
   }
 }
 
-const config = configs.ledStrip;
-
+const config = configs.ledMatrix;
 
 // Helper functions
 const getHex = (str) => {
+  const convertTo32 = (config.is32Bits) ? '00': '';
   const hexNumber = (("0000000" + ((str|0)+4294967296).toString(16)).substr(-8)).slice(0, -2);
-  return (config.rgb) ? getRGB(hexNumber): `0x${hexNumber}`;
+  return `0x${convertTo32}${hexNumber}`;
 };
-const getRGB = (hex) => {
-  return `{0x${hex.substr(0, 2)}, 0x${hex.substr(2, 2)}, 0x${hex.substr(4, 2)}}`
-}
+
 const checkExtension = (fileName) => {
   const validExt = ['png', 'jpg', 'bmp'];
   const ext = fileName.split('.').pop();
@@ -37,20 +37,36 @@ const sortByKey = (array, key) => {
   });
 };
 
+const getImageColors = (str, img, x, y) => `${str}${getHex(img.getPixelColor(x, y))}, `;
+
 const getPixels = (image) => {
   const imageWidth = image.bitmap.width;
   const imageHeight = image.bitmap.height;
   let imageColors = '';
+  let imageColorsA = '';
+  let imageColorsB = '';
   for (let y = 0; y < imageHeight; y++) {
     if (y !== 0 && y % 2 !== 0 && config.zigzag) {
-      for (let x = imageWidth - 1; x >= 0; x--) imageColors = `${imageColors}${getHex(image.getPixelColor(x, y))}, `;
+      for (let x = imageWidth - 1; x >= 0; x--) {
+        imageColors = getImageColors(imageColors, image, x, y);
+        if (y < 32) imageColorsA = getImageColors(imageColorsA, image, x, y);
+               else imageColorsB = getImageColors(imageColorsA, image, x, y);
+      }
     } else {
-      for (let x = 0; x < imageWidth; x++) imageColors = `${imageColors}${getHex(image.getPixelColor(x, y))}, `;
+      for (let x = 0; x < imageWidth; x++) {
+        imageColors = getImageColors(imageColors, image, x, y);
+        if (y < 32) imageColorsA = getImageColors(imageColorsA, image, x, y);
+               else imageColorsB = getImageColors(imageColorsA, image, x, y);
+      }
     }
     imageColors = `${imageColors}\n`;
+    if (y < 32) imageColorsA = `${imageColorsA}\n`;
+           else imageColorsB = `${imageColorsB}\n`;
   }
   imageColors = imageColors.slice(0, -3);
-  return imageColors;
+  imageColorsA = imageColorsA.slice(0, -3);
+  imageColorsB = imageColorsB.slice(0, -3);
+  return { imageColors, imageColorsA, imageColorsB };
 };
 const getArrayOfPixels = (filePath) => {
   return Jimp.read(filePath)
@@ -59,31 +75,39 @@ const getArrayOfPixels = (filePath) => {
         totalSize: (image.bitmap.width * image.bitmap.height),
         width: image.bitmap.width,
         height: image.bitmap.height,
-        colorString: getPixels(image),
+        colorString: getPixels(image).imageColors,
+        colorStringA: getPixels(image).imageColorsA,
+        colorStringB: getPixels(image).imageColorsB,
       }
     })
     .catch(err => { throw err });
 }
 
 // Core functions
-const generateFiles = async (pixelImages) => {
-  Object.keys(pixelImages).map(animationName => {
+const generateFileString = (filePart, animationName, pixelImages, splitDivisor) => {
     let propertiesVar = '';
-    const rgbDef = (config.rgb) ? '[3]' : '';
-    const typeVar = (config.rgb) ? 'uint8_t' : 'long';
-    let hFile = `const ${typeVar} ${animationName}[][${pixelImages[animationName][0].totalSize}]${rgbDef} PROGMEM = {\n`;
+    let hFile = `const long ${animationName}[][${pixelImages[animationName][0].totalSize/splitDivisor}] PROGMEM = {\n`;
     const sortedFrames = sortByKey(pixelImages[animationName], 'fileName');
     sortedFrames.map(frame => {
-      hFile = `${hFile}{\n${frame.colorString}},\n`;
+      hFile = `${hFile}{\n${frame[`colorString${filePart}`]}},\n`;
       propertiesVar = `
         \nconst int ${animationName}Width = ${frame.width};
-        \nconst int ${animationName}Height = ${frame.height};
+        \nconst int ${animationName}Height = ${frame.height / splitDivisor};
       `;
     });
     hFile = `${hFile.slice(0, -3)}\n}\n};`;
     hFile = `${hFile}\nconst int ${animationName}Frames = ${sortedFrames.length};${propertiesVar}`;
-    fs.writeFileSync(`${config.imagePath}${animationName}/${animationName}.h`, hFile); 
-    console.log(`Saved: ${config.imagePath}${animationName}/${animationName}.h`);
+    fs.writeFileSync(`${config.imagePath}${animationName}/${animationName}${filePart}.h`, hFile); 
+    console.log(`Saved: ${config.imagePath}${animationName}/${animationName}${filePart}.h`);
+}
+
+const generateFiles = async (pixelImages) => {
+  Object.keys(pixelImages).map(animationName => {
+    generateFileString('', animationName, pixelImages, 1);
+    if (config.splitImages) {
+      generateFileString('A', animationName, pixelImages, 2);
+      generateFileString('B', animationName, pixelImages, 2);
+    }
   });
 };
 const walkDirectories = function (dir, animationName = '') {
@@ -98,13 +122,7 @@ const walkDirectories = function (dir, animationName = '') {
       promises.push(
         getArrayOfPixels(filePath).then(imageInfo => {
           if (!pixelImages[animationName]) pixelImages[animationName] = [];
-          pixelImages[animationName].push({
-            fileName,
-            totalSize: imageInfo.totalSize,
-            width: imageInfo.width,
-            height: imageInfo.height,
-            colorString: imageInfo.colorString
-          });
+          pixelImages[animationName].push({ ...imageInfo, fileName });
         })
       );
     }
