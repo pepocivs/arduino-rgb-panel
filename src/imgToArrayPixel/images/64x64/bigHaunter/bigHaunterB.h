8df31a5787ca11bef0e7b577590b8833febd5091
Jorge Martín <tcbpepo@gmail.com>
2019-07-30 23:08:48 +0200

    chore: adding new parts

---
+++ src/imgToArrayPixel/images/64x64/bigHaunter/bigHaunterB.h
@@ -0,0 +1,13 @@
+const long bigHaunter[][1024] PROGMEM = {
+{
+},
+{
+
+}
+};
+const int bigHaunterFrames = 2;
+        
+const int bigHaunterWidth = 64;
+        
+const int bigHaunterHeight = 16;
+      
