b58cd862f42c2a3dbec03929ce0035aec989fdb4
PepoCivs <tcbpepo@gmail.com>
2019-08-26 02:17:12 +0200

    chore: adding first approach to pixelMatrix 64x64

--- src/rgbMatrix/WifiModule/wifi-code/wifi-code.ino
+++ src/rgbMatrix/WifiModule/wifi-code/wifi-code.ino
@@ -15,31 +15,28 @@ Ticker display_ticker;
 int width = 128;
 int height = 32;
 
+// This defines the 'on' time of the display is us. The larger this number,
+// the brighter the display. If too large the ESP will crash
+uint8_t display_draw_time=70; //10-50 is usually fine
+
+int timeBetween = 5000;
+
 PxMATRIX display(width,height,P_LAT, P_OE,P_A,P_B,P_C,P_D);
 
-uint16_t myRED = display.color565(255, 0, 0);
-uint16_t myGREEN = display.color565(0, 255, 0);
-uint16_t myBLUE = display.color565(0, 0, 255);
-uint16_t myWHITE = display.color565(255, 255, 255);
-uint16_t myYELLOW = display.color565(255, 255, 0);
-uint16_t myCYAN = display.color565(0, 255, 255);
-uint16_t myMAGENTA = display.color565(255, 0, 255);
-uint16_t myORANGE = display.color565(255, 120, 0);
-
-
 #include "kindomHearts.h"
-#include "cromatic.h"
+#include "escudoGiner.h"
 #include "guinness.h"
+#include "zelda.h"
+#include "bigtriforce.h"
 
 void display_updater()
 {
-  display.display(70);
+  display.display(display_draw_time);
 }
 void setup() {
   Serial.begin(9600);
   display.begin(16);
   display.clearDisplay();
-
   Serial.print("Pixel draw latency in us: ");
   unsigned long start_timer = micros();
   display.drawPixel(1, 1, 0);
@@ -52,37 +49,39 @@ void setup() {
   delta_timer = micros() - start_timer;
   Serial.println(delta_timer);
 
-
   display_ticker.attach(0.002, display_updater);
   yield();
-
-  
   display.clearDisplay();
-  showAnimation(guinness0);
-  //display.drawPixel(0, 0, myRED);
-  //display.drawPixel(0, 16, myGREEN);
-  //display.drawPixel(0, 32, myBLUE);
-
-  //display.drawPixel(16, 0, myRED);
-  //display.drawPixel(16, 16, myGREEN);
-  //display.drawPixel(16, 32, myBLUE);
-  //display.drawPixel(16, 63, myWHITE);
-  
-  //display.drawPixel(16, 0, myYELLOW);
-  //display.drawPixel(31, 0, myCYAN);
-  //display.drawPixel(31, 0, myCYAN);
-  //display.drawPixel(63, 0, myMAGENTA);
-  //display.drawPixel(31, 31, myORANGE);
-
-  //display.drawPixel(70, 70, myORANGE);
 
 }
 
 void loop() {
-  
+  display.setBrightness(255);
+  showAnimation(kindomHearts, kindomHeartsFrames, 220);
+  delay(timeBetween);
+  showAnimation(zelda, zeldaFrames, 220);
+  delay(timeBetween);
+  showAnimation(escudoGiner, escudoGinerFrames, 220);
+  delay(timeBetween);
+  showAnimation(guinness, guinnessFrames, 220);
+  delay(timeBetween);
+  repeatAnimation(bigtriforce, bigtriforceFrames, 250, 3);
 }
 
-void showAnimation(const long animation[4096]) {
+void repeatAnimation(const long animation[][4096], int nFrames, int delayTime, int times) {
+  for (int sequence = 0; sequence < times; sequence++) {
+    showAnimation(animation, nFrames, delayTime);
+  }
+}
+
+void showAnimation(const long animation[][4096], int nFrames, int delayTime) {
+  for (int frame = 0; frame < nFrames; frame++) {
+    showFrame(animation[frame]);
+    delay(delayTime);
+  }
+}
+
+void showFrame(const long animation[4096]) {
   int pixelCount = 0;
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
