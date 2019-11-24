b56c10ab03b5e9a3705a7fcc602b3cd32f3639f5
PepoCivs <tcbpepo@gmail.com>
2019-11-24 01:27:54 +0100

    chore: adding navidad animation

---
+++ src/imgToArrayPixel/images/64x64/navidad/navidadB.h
@@ -0,0 +1,25 @@
+const long navidad[][2048] PROGMEM = {
+{
+},
+{
+},
+{
+},
+{
+},
+{
+},
+{
+},
+{
+},
+{
+
+}
+};
+const int navidadFrames = 8;
+        
+const int navidadWidth = 128;
+        
+const int navidadHeight = 32;
+      
